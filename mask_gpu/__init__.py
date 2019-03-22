3331808389d22427bbde3bca3115db7a2e85a1f7
Saravanabalagi R <saravanabalagi@hotmail.com>
2019-03-22 12:23:44 +0000

    Adding Unmask All Adding Reverse Adding Console Log Print Command Don't Set env inside of Python since they wont persist

--- mask_gpu/__init__.py
+++ mask_gpu/__init__.py
@@ -4,29 +4,46 @@ __version__ = "0.0.1"
 import subprocess as sp
 import os
 
-def mask_unused_gpus(expose=1, min_memory=1024):
-  available_gpus = get_available_gpus(min_memory)
-  print('---------------------')
-  if len(available_gpus) < expose:
-    print('Found only {} usable GPU(s) in the system'.format( len(available_gpus) ))
-    print('Can\'t expose {} GPU(s)'.format(expose))
-    return
-  set_visible_devices = ','.join(map(str, available_gpus[:expose]))
-  print('Exposing {} GPU(s)...'.format(expose))
-  os.environ["CUDA_VISIBLE_DEVICES"] = set_visible_devices
-  print('Visible GPUs: {} -> {}'.format(len(set_visible_devices), available_gpus[:expose]))
+def mask_unused_gpus(expose=1, min_memory=1024, reverse=False, unmask_all=False, console_log=False):
 
-def get_available_gpus(min_memory=1024):
+  if unmask_all:
+      command = 'export CUDA_VISIBLE_DEVICES='
+      print(command)
+      return
+
+  available_gpus = get_available_gpus(min_memory, reverse, console_log=False)
+  if len(available_gpus) < expose:
+    echo_string = 'echo '
+    if console_log: echo_string = ""
+    print('{}Found only {} usable GPU(s) with atleast {}MiB free memory in the system; Can\'t expose {} GPU(s)'.format( echo_string, len(available_gpus), min_memory, expose ))
+    return
+
+  set_visible_devices = ','.join(map(str, available_gpus[:expose]))
+  command = 'export CUDA_VISIBLE_DEVICES={}'.format(set_visible_devices)
+
+  if console_log:
+      print('Generating command for exposing {} GPU(s)...'.format(expose))
+      print('Visible GPUs will be: {} -> {}'.format(len(set_visible_devices), available_gpus[:expose]))
+      print('=========================================')
+      print('Run the following command in your shell:')
+      print("  {}".format(command))
+      print('=========================================')
+      print()
+
+  else: print(command)
+  return
+
+def get_available_gpus(min_memory=1024, reverse=False, console_log=True):
   COMMAND = "nvidia-smi --query-gpu=memory.free --format=csv"
 
   try:
-    print('Finding GPUs with a minimum of {}MiB free memory...'.format(min_memory))
+    if console_log: print('Finding GPUs with a minimum of {}MiB free memory...'.format(min_memory))
     _output_to_list = lambda x: x.decode('ascii').split('\n')[:-1]
     memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
     memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
     available_gpus = [i for i, x in enumerate(memory_free_values) if x > min_memory]
-    print('GPUs available: {} -> {}'.format(len(available_gpus), available_gpus))
-    return available_gpus
+    if console_log: print('GPUs available: {} -> {}'.format(len(available_gpus), available_gpus))
+    return sorted(available_gpus, reverse=reverse)
 
   except Exception as e:
     print('"nvidia-smi" is probably not installed. Could not retrieve GPU information', e)
--- mask_gpu/command_line.py
+++ mask_gpu/command_line.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from mask_gpu import __version__
 import argparse
+import os
 
 from mask_gpu import get_available_gpus
 from mask_gpu import mask_unused_gpus
@@ -8,17 +9,19 @@ from mask_gpu import mask_unused_gpus
 
 def main():
     parser = argparse.ArgumentParser(prog='mask-gpu', description='Mask GPUs and expose only necessary GPUs so tensorflow will not use them')
-    sp = parser.add_mutually_exclusive_group()
-    sp.add_argument('-i', '--info', action='store_true', help='show free gpu with minimum available specified memory')
-    sp.add_argument('-e', '--expose', default=1, type=int, help='number of free GPUs to expose (or leave unmasked)', metavar='')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-u', '--unmask-all', action='store_true', help='unmask all GPUs; revert')
+    group.add_argument('-i', '--info', action='store_true', help='show free gpu with minimum available specified memory')
+    group.add_argument('-e', '--expose', default=1, type=int, help='number of free GPUs to expose (or leave unmasked)', metavar='')
+    parser.add_argument('-r', '--reverse', action='store_true', help='get GPU numbers in descending order')
     parser.add_argument('-m', '--min-memory', default=1024, type=int, help='minimum memory to consider a GPU free', metavar='')
+    parser.add_argument('-c', '--console-log', action='store_true', help='logs info in stdout, useful for visually seeing info')
     parser.add_argument('-v', '--version', action='version',
                                 version='%(prog)s {version}'.format(version=__version__))
 
     args = parser.parse_args()
-
-    if(args.info): get_available_gpus(min_memory=args.min_memory)
-    else: mask_unused_gpus(expose=args.expose, min_memory=args.min_memory)
+    if(args.info): get_available_gpus(min_memory=args.min_memory, reverse=args.reverse)
+    else: mask_unused_gpus(expose=args.expose, min_memory=args.min_memory, reverse=args.reverse, unmask_all=args.unmask_all, console_log=args.console_log)
 
 
 if __name__ == '__main__':
